b2a96b221ec657a3249021a2f5e2fe62e58ae8ef
cobotwong1 <won23001@byui.edu>
2023-04-24 22:00:41 +0800

    Learning Activity - C# Prep 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,63 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your percentage of grade? ");
+        string value = Console.ReadLine();
+        int grade = int.Parse(value);
+        
+        string letter = "";
+        if (grade >= 90)
+        {
+            letter = "A";
+        } 
+        else if(grade >= 80)
+        {
+            letter = "B";
+        }
+        else if(grade >= 70)
+        {
+            letter = "C";
+        }
+        else if(grade >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        int symbol = grade % 10;
+        string sign = "";
+        if (grade >= 93|| grade <60)
+        {
+            sign = "";
+        }
+        else
+        {
+            if (symbol >= 7)
+            {
+                sign = "+";
+            }
+            else if (symbol <= 2)
+            {
+                sign = "-";
+            }   
+            else
+            {
+                sign = "";
+            }
+        }
+
+        Console.WriteLine($"Your grading is {letter}{sign}");
+        Console.WriteLine("");
+        if (grade >= 70)
+        {
+            Console.WriteLine("Congratulation! You did a great job!");
+        }
+        else
+        {
+            Console.WriteLine("Do better next time!");
+        }
     }
 }
