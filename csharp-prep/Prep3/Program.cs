c3bba63c7e2af398a5d4e66d46ee0bf3873d566d
cobotwong1 <won23001@byui.edu>
2023-05-05 19:43:09 +0800

    prep3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -3,7 +3,21 @@ using System;
 class Program
 {
     static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Prep3 World!");
+    { 
+    Random randomGenerator = new Random();
+    int number = randomGenerator.Next(1,100);
+    int guess = 0;
+    while (guess != number)
+        {
+        Console.Write("What is your guess? ");
+        string answer = Console.ReadLine();
+        guess = int.Parse(answer);
+        }
+        if (guess < number){
+            Console.WriteLine("Higher");
+        }
+        else if (number < guess){
+            Console.WriteLine("Lower");
+        }
+        }
     }
-}
